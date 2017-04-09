ac6f1edc4b2642d09767c0f30fa5cccc30dcc8a7
Ripul Jain <r6jain@uwaterloo.ca>
2017-04-09 02:54:25 -0400

    added tweets for model

--- LieDetector/src/main/scala/uwaterloo/App.scala
+++ LieDetector/src/main/scala/uwaterloo/App.scala
@@ -1,8 +1,7 @@
 package uwaterloo
 import java.nio.file.Files
-import java.nio.charset.Charset
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf}
 import org.apache.spark.streaming._
 
 import scala.io.Source
@@ -10,32 +9,17 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
 import scala.collection.mutable
-import scala.collection.immutable.Queue
 import jep.Jep
 import jep.JepConfig
 
-case class TweetInfo (
-                      date: String,
-                      subject: String,
-                      topic: String,
-                      stance: String
-                    ) extends Serializable
-
-class FiniteQueue[A](q: Queue[A]) {
-  def enqueueFinite[B >: A](elem: B): Queue[B] = {
-    var ret = q.enqueue(elem)
-    while (ret.size > 10) { ret = ret.dequeue._2 }
-    ret
-  }
-}
-
 /**
- * Naive FD violation detection
+  * FD violation detection
   * @author Ripul
  */
 object App {
 
-  def mappingFunction(key: (String,String), value: Option[(String,String)], state: State[Map[(String,String),(String,String)]])
+  def mappingFunction(key: (String,String), value: Option[(String,String,String)],
+                      state: State[Map[(String, String),(String,String)]])
   //: Option[Map[(String,String),(String,String)]] = {
   : Option[String] = {
 
@@ -45,19 +29,40 @@ object App {
         .getOption()
         .getOrElse(Map[(String,String) , (String,String)]())
 
+    // This RDD wil be populated with
+    // all FD violations
+    val violationDetection: Option[String] =
+    value
+      .map(x => {
+        if(existingTweets.contains(key)) {
+          val v = existingTweets(key)
+          if (!v._2.contentEquals(x._3)) {
+            key.toString() + " stance changed"
+          }
+          else{""}
+        }
+        else{
+          ""
+        }
+      })
+
     var mustStore = false
     val jepcon = new JepConfig()
     jepcon.addSharedModules("numpy")
     val jep = new Jep(jepcon)
     jep.runScript("../src/classify.py")
-    var s = "John McCain idiot"
-    val res = jep.invoke("classify", s).asInstanceOf[String].replace("']","")
+    var currentTweet = value.get._3
+    val res = jep.invoke("classify", currentTweet).asInstanceOf[String].replace("']","")
     val classifyResult = res.replace("['","")
 
     // If the model says store, we store
     // this tweet
     if(classifyResult.equals("yes")){
       mustStore = true
+      println(key._1 + ", " + key._2 + ", stored " + " - " + currentTweet)
+    }
+    else{
+      println(key._1 + ", " + key._2 + ", NOT stored " + " - " + currentTweet)
     }
     jep.close()
 
@@ -65,30 +70,14 @@ object App {
     // updated hold
     val updatedHold: Map[(String,String),(String,String)] =
       value
-        .map(x => if(mustStore) {
-          existingTweets.updated(key,x)
-        }
-        else{
-          existingTweets
-        })
-        .getOrElse(existingTweets)
-
-    // This RDD wil be populated with
-    // all FD violations
-    val violationDetection: Option[String] =
-      value
-        .map(x => {
-          if(existingTweets.contains(key)) {
-            val v = existingTweets.get(key).get
-            if (!v._2.contentEquals(x._2)) {
-              key.toString() + " stance changed"
-            }
-            else{""}
+        .map(x =>
+          if(mustStore) {
+            existingTweets.updated(key,(x._1,x._2))
           }
           else{
-            ""
-          }
-        })
+            existingTweets
+          })
+        .getOrElse(existingTweets)
 
     // Solidify the updated hold as new state
     state.update(updatedHold)
@@ -107,14 +96,14 @@ object App {
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
-    val lines = mutable.Queue[RDD[((String,String),(String,String))]]()
+    val lines = mutable.Queue[RDD[((String,String),(String,String,String))]]()
     val dstream = ssc.queueStream(lines)
     val filename = "../data/targetdata_stream.orig"
 
     try{
       for (line <- Source.fromFile(filename).getLines()) {
         val arr = line.split(",")
-        lines += ssc.sparkContext.makeRDD(Seq((arr(1),arr(2))->(arr(0),arr(3))))
+        lines += ssc.sparkContext.makeRDD(Seq((arr(1),arr(2))->(arr(0),arr(4),arr(3))))
       }
     } catch {
       case ex: Exception => println("Bummer, an exception happened: " + ex.toString())
@@ -130,9 +119,7 @@ object App {
 
     //dstream.print()
     ssc.start()
-    ssc.awaitTerminationOrTimeout(15000)
+    ssc.awaitTerminationOrTimeout(60000)
     ssc.stop()
-
-    //print("Queue length: " + states.length.toString)
   }
 }
